15654442bfdc87f26ba3cd7ff3e1cd46736fb8f1
lothanhdat <lothanhdat95@gmail.com>
2024-03-21 14:10:54 +0700

    Init project

---
+++ src/main/java/com/nhom3/ecommerceadmin/ECommerceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.nhom3.ecommerceadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhom3/ecommerceadmin/ECommerceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom3.ecommerceadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
